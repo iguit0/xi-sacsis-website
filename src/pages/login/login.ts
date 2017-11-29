9422cacfad26812c0ba025bfb666ea1b481fb793
Igor Alves <igorsk89@gmail.com>
2017-11-29 01:14:44 -0200

    Login reformulado

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,6 @@
 import { SignupPage } from '../signup/signup';
-import { Component,ViewChild } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams,LoadingController,AlertController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -9,34 +9,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public loadingCtrl: LoadingController,public alertCtrl: AlertController) {
   }
 
-  @ViewChild('player') player;
-  
-    // It's interesting to remove the src and put it back
-    // when entering and leaving the page so there are no memory leaks.
-    ionViewWillLeave() {
-      // the .nativeElement property of the ViewChild is the reference to the tag <video>
-      this.player.nativeElement.src = '';
-      this.player.nativeElement.load();
-    }
-  
-    ionViewWillEnter() {
-      this.player.nativeElement.src = 'assets/video/background-480.mp4';
-      this.player.nativeElement.load();
-    }
-  
-    ionViewDidLoad() {
-      console.log('Hello LoginBackgroundVideo Page');
-    }
-  
-    goToSignup() {
-      this.navCtrl.setRoot(SignupPage, { animate: true, direction: 'forward' });
-    }
-  
-    goToLogin() {
-      console.log('Login clicked');
-    }
+  login() {
+    const loading = this.loadingCtrl.create({
+      duration: 500
+    });
+
+    loading.onDidDismiss(() => {
+      const alert = this.alertCtrl.create({
+        title: 'Logged in!',
+        subTitle: 'Thanks for logging in.',
+        buttons: ['Dismiss']
+      });
+      alert.present();
+    });
+    loading.present();
+  }
+
+  goToSignup(){
+    this.navCtrl.push(SignupPage, {animation: 'ios-transition'});
+  }
 
 }
--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
 
 @IonicPage()
 @Component({
@@ -8,8 +9,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,public loadingCtrl: LoadingController,
+    public alertCtrl: AlertController) {
   }
 
+  login() {
+    const loading = this.loadingCtrl.create({
+      duration: 500
+    });
+
+    loading.onDidDismiss(() => {
+      const alert = this.alertCtrl.create({
+        title: 'Logged in!',
+        subTitle: 'Thanks for logging in.',
+        buttons: ['Dismiss']
+      });
+      alert.present();
+    });
+    loading.present();
+  }
+
+  goToLogin(){
+    this.navCtrl.setRoot(LoginPage, {animation: 'wp-transition'});
+  }
 
 }
