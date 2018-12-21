d9017ab1f69533741b68096a61c91b8be10f9a8b
Igor Alves <igorsk89@gmail.com>
2018-12-21 11:18:15 -0200

    Redux & React-Router

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Quick Life
+      </div>
+    );
+  }
+}
+
+export default App;
