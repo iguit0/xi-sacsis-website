899b4c98f87c622a6538cc65af35cbea80b9d2d4
Igor Alves <igorsk89@gmail.com>
2018-12-23 22:49:24 -0200

    Show Hide Pass SignIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Form, Container } from "./styles";
-
+import { InputGroup, InputGroupAddon, InputGroupText, Input } from "reactstrap";
 import Logo from "../../assets/logo.svg";
 
 class SignIn extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      show: false,
       email: "",
       password: "",
       error: ""
@@ -25,6 +26,10 @@ class SignIn extends Component {
     }
   };
 
+  togglePass = () => {
+    this.setState({ show: !this.state.show });
+  };
+
   render() {
     return (
       <Container>
@@ -32,16 +37,38 @@ class SignIn extends Component {
           <img src={Logo} alt="QuickLife logo" />
           <h2>Login</h2>
           {this.state.error && <p>{this.state.error}</p>}
-          <input
+          <Input
             type="email"
             placeholder="E-mail"
             onChange={e => this.setState({ email: e.target.value })}
           />
-          <input
-            type="password"
-            placeholder="Senha"
-            onChange={e => this.setState({ password: e.target.value })}
-          />
+          <InputGroup className="mb-3">
+            {this.state.show ? (
+              <Input
+                type="text"
+                placeholder="Senha"
+                onChange={e => this.setState({ password: e.target.value })}
+              />
+            ) : (
+              <Input
+                type="password"
+                placeholder="Senha"
+                onChange={e => this.setState({ password: e.target.value })}
+              />
+            )}
+            <InputGroupAddon
+              addonType="append"
+              onClick={() => this.togglePass()}
+            >
+              <InputGroupText>
+                {this.state.show ? (
+                  <i className="fa fa-eye" />
+                ) : (
+                  <i className="fa fa-eye-slash" />
+                )}
+              </InputGroupText>
+            </InputGroupAddon>
+          </InputGroup>
           <button type="submit">Entrar</button>
           <hr />
           <Link to="/registrar">Não tem conta? Registre-se agora!</Link>
