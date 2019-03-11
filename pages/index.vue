eda65899cd9bc7b1c1f7971cf918b7c23b1af527
Igor Alves <igorsk89@gmail.com>
2019-03-11 02:40:17 -0300

    fonte source sans pro

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,15 @@
 <template>
-  <section class="container">
-    <div>
-      <h1 class="title">CTLP</h1>
-      <h2 class="subtitle">Clube de Tiro Lagoa da Prata</h2>
-      <hr>
-      <b-btn to="/admin" variant="warning" class="mr-1">Admin</b-btn>
-      <b-btn to="/sobre-nos" variant="primary" class="mr-1">Sobre nós</b-btn>
-      <b-btn to="/noticias" variant="success" class="mr-1">Notícias</b-btn>
-    </div>
-  </section>
+  <div class="home">
+    <h1 class="title">CTLP</h1>
+    <h2 class="subtitle">Clube de Tiro Lagoa da Prata</h2>
+    <hr>
+    <b-btn to="/admin" variant="warning" class="mr-1">Admin</b-btn>
+    <b-btn to="/sobre-nos" variant="primary" class="mr-1">Sobre nós</b-btn>
+    <b-btn to="/noticias" variant="success" class="mr-1">Notícias</b-btn>
+  </div>
 </template>
 
 <style scoped>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
 .title {
   font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
     "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
