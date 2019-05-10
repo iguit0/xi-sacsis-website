edf9a02cf3a6c253ac00864f27a43b9dcde6bba6
Luis <luisedu.soares@outlook.com>
2019-05-10 10:39:23 -0300

    build folder

---
+++ dist/precache-manifest.4223f5a66dcf9066bf019d94cceceaf8.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9d7a778824165b7d4d1c74386285906",
+    "url": "/img/apple-icon-72x72.png"
+  },
+  {
+    "revision": "6e217f769ce2722460f1aee6bf17b711",
+    "url": "/img/ms-icon-310x310.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "93e661e51c4a7754532f",
+    "url": "/js/chunk-13ef54f2.55dc08eb.js"
+  },
+  {
+    "revision": "ffbde975f69e88bf99c5cde44490c715",
+    "url": "/img/ms-icon-70x70.png"
+  },
+  {
+    "revision": "d491218fbe6ebf6f98c9",
+    "url": "/js/chunk-1a4d6c1c.d98c43d1.js"
+  },
+  {
+    "revision": "375ca6d22cd83773d8fb",
+    "url": "/js/app.74840fe7.js"
+  },
+  {
+    "revision": "356e72339471a36abdbd",
+    "url": "/js/chunk-bb75a2b4.7b111f0a.js"
+  },
+  {
+    "revision": "5787c9665c6fe442e34434eb5d677c99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72873a1d85a82af08f6e",
+    "url": "/js/chunk-vendors.76bb7738.js"
+  },
+  {
+    "revision": "50193c17d4e12ac1d49347bf403b3b43",
+    "url": "/img/ufv-logo.50193c17.png"
+  },
+  {
+    "revision": "18792b61d153693266a89ca8b080942c",
+    "url": "/img/parallax.18792b61.png"
+  },
+  {
+    "revision": "36d4b87514744f6fc41e996963b465cd",
+    "url": "/img/favicon-96x96.png"
+  },
+  {
+    "revision": "22a620696ea2feea2b3d63ec1dc94586",
+    "url": "/img/logo-infoalto.22a62069.png"
+  },
+  {
+    "revision": "c194ab848a221433df75753e144e7e6e",
+    "url": "/img/ms-icon-150x150.png"
+  },
+  {
+    "revision": "5761d3df574ce2f409f37f431828fe8e",
+    "url": "/img/ms-icon-144x144.png"
+  },
+  {
+    "revision": "54ecf212ce46474f6553bafa02256fb3",
+    "url": "/img/apple-icon-120x120.png"
+  },
+  {
+    "revision": "fc2955fa81ee41c1a02dd17add77240b",
+    "url": "/img/android-icon-36x36.png"
+  },
+  {
+    "revision": "dcad51b9434c5eeb4ac335e33d8a6024",
+    "url": "/img/apple-icon-57x57.png"
+  },
+  {
+    "revision": "0bc7d3d02113b3a5c08269e12cb52110",
+    "url": "/img/apple-icon-60x60.png"
+  },
+  {
+    "revision": "473a2747bb20ece3a014122b6f0eacc6",
+    "url": "/img/apple-icon-114x114.png"
+  },
+  {
+    "revision": "1ec9a3d3254ec40a9ab7ce66130e1b8a",
+    "url": "/img/favicon-16x16.png"
+  },
+  {
+    "revision": "bbb6926cabd14e3acf805856b1a3b7ca",
+    "url": "/img/favicon-32x32.png"
+  },
+  {
+    "revision": "2826b7d95ec774dd3a6a2b0f81fa9b9a",
+    "url": "/img/apple-icon-180x180.png"
+  },
+  {
+    "revision": "36d4b87514744f6fc41e996963b465cd",
+    "url": "/img/android-icon-96x96.png"
+  },
+  {
+    "revision": "73ba007214e8924b997ce933c7f8f98b",
+    "url": "/img/apple-icon-76x76.png"
+  },
+  {
+    "revision": "4abbbb80059fd1abb3fdba62613a1dd2",
+    "url": "/img/casi.4abbbb80.png"
+  },
+  {
+    "revision": "af1e2009ecad1f0f49da1b75cd74cb08",
+    "url": "/img/apple-icon.png"
+  },
+  {
+    "revision": "f9d7a778824165b7d4d1c74386285906",
+    "url": "/img/android-icon-72x72.png"
+  },
+  {
+    "revision": "5761d3df574ce2f409f37f431828fe8e",
+    "url": "/img/apple-icon-144x144.png"
+  },
+  {
+    "revision": "01cd7a3e87eb80431b877af2de9b6ea9",
+    "url": "/img/apple-icon-152x152.png"
+  },
+  {
+    "revision": "272425dc956ec4befa460c6dbf8c165a",
+    "url": "/img/android-icon-48x48.png"
+  },
+  {
+    "revision": "af1e2009ecad1f0f49da1b75cd74cb08",
+    "url": "/img/apple-icon-precomposed.png"
+  },
+  {
+    "revision": "78728730c6f498b12bbaeaf8dc0c3cfd",
+    "url": "/img/android-icon-192x192.png"
+  },
+  {
+    "revision": "5761d3df574ce2f409f37f431828fe8e",
+    "url": "/img/android-icon-144x144.png"
+  },
+  {
+    "revision": "72873a1d85a82af08f6e",
+    "url": "/css/chunk-vendors.8dd93730.css"
+  },
+  {
+    "revision": "356e72339471a36abdbd",
+    "url": "/css/chunk-bb75a2b4.2c284151.css"
+  },
+  {
+    "revision": "d491218fbe6ebf6f98c9",
+    "url": "/css/chunk-1a4d6c1c.d4e60883.css"
+  },
+  {
+    "revision": "93e661e51c4a7754532f",
+    "url": "/css/chunk-13ef54f2.2e34ed75.css"
+  },
+  {
+    "revision": "375ca6d22cd83773d8fb",
+    "url": "/css/app.00dd635b.css"
+  }
+];
