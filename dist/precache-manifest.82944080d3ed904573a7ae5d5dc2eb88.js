40900c993cca9c6b6cdc8113fed372a80fad69d2
Igor Alves <igorsk89@gmail.com>
2019-05-11 15:12:12 -0300

    new version

--- dist/precache-manifest.82944080d3ed904573a7ae5d5dc2eb88.js
+++ dist/precache-manifest.82944080d3ed904573a7ae5d5dc2eb88.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
-    "revision": "f9d7a778824165b7d4d1c74386285906",
-    "url": "/img/apple-icon-72x72.png"
+    "revision": "0bc7d3d02113b3a5c08269e12cb52110",
+    "url": "/img/apple-icon-60x60.png"
   },
   {
     "revision": "6e217f769ce2722460f1aee6bf17b711",
@@ -20,23 +20,31 @@ self.__precacheManifest = [
     "url": "/img/ms-icon-70x70.png"
   },
   {
-    "revision": "e7d62d3267ef8c62dd42",
-    "url": "/js/chunk-59b24cff.c41ff249.js"
+    "revision": "c2835fc693502b023963",
+    "url": "/js/chunk-40d08593.da74e6f3.js"
   },
   {
-    "revision": "94448cfa72594d756f53",
-    "url": "/js/app.af9394b8.js"
+    "revision": "5cae1215b884e9d29210",
+    "url": "/js/app.bf2f8e8a.js"
   },
   {
     "revision": "a48632deb1f77d3f6d9a",
     "url": "/js/chunk-7c3900fd.7d6b0700.js"
   },
   {
-    "revision": "7424e1691cce5ec55aaba5e3e57ecc36",
+    "revision": "0359365a7f687ebfa378a3dcbb037019",
     "url": "/index.html"
   },
   {
-    "revision": "39138eb71bec36414a2a",
+    "revision": "ed200db68e5327e09202",
+    "url": "/js/chunk-9acbd058.6ca882e2.js"
+  },
+  {
+    "revision": "1138106d86b1c1d0a8b0c7df8b62b82a",
+    "url": "/img/parallax.1138106d.png"
+  },
+  {
+    "revision": "3183b3e7ed84ae8c08d4",
     "url": "/js/chunk-vendors.8e115ba1.js"
   },
   {
@@ -44,12 +52,8 @@ self.__precacheManifest = [
     "url": "/img/ufv-logo.563271c8.png"
   },
   {
-    "revision": "1138106d86b1c1d0a8b0c7df8b62b82a",
-    "url": "/img/parallax.1138106d.png"
-  },
-  {
-    "revision": "73ba007214e8924b997ce933c7f8f98b",
-    "url": "/img/apple-icon-76x76.png"
+    "revision": "36d4b87514744f6fc41e996963b465cd",
+    "url": "/img/android-icon-96x96.png"
   },
   {
     "revision": "3637b9b37f0fb813931cfa51457a11ad",
@@ -64,16 +68,16 @@ self.__precacheManifest = [
     "url": "/img/ms-icon-144x144.png"
   },
   {
-    "revision": "473a2747bb20ece3a014122b6f0eacc6",
-    "url": "/img/apple-icon-114x114.png"
+    "revision": "36d4b87514744f6fc41e996963b465cd",
+    "url": "/img/favicon-96x96.png"
   },
   {
-    "revision": "272425dc956ec4befa460c6dbf8c165a",
-    "url": "/img/android-icon-48x48.png"
+    "revision": "fc2955fa81ee41c1a02dd17add77240b",
+    "url": "/img/android-icon-36x36.png"
   },
   {
-    "revision": "0bc7d3d02113b3a5c08269e12cb52110",
-    "url": "/img/apple-icon-60x60.png"
+    "revision": "f9d7a778824165b7d4d1c74386285906",
+    "url": "/img/android-icon-72x72.png"
   },
   {
     "revision": "dcad51b9434c5eeb4ac335e33d8a6024",
@@ -84,12 +88,16 @@ self.__precacheManifest = [
     "url": "/img/apple-icon.png"
   },
   {
-    "revision": "1ec9a3d3254ec40a9ab7ce66130e1b8a",
-    "url": "/img/favicon-16x16.png"
+    "revision": "f9d7a778824165b7d4d1c74386285906",
+    "url": "/img/apple-icon-72x72.png"
   },
   {
     "revision": "bbb6926cabd14e3acf805856b1a3b7ca",
     "url": "/img/favicon-32x32.png"
+  },
+  {
+    "revision": "1ec9a3d3254ec40a9ab7ce66130e1b8a",
+    "url": "/img/favicon-16x16.png"
   },
   {
     "revision": "af1e2009ecad1f0f49da1b75cd74cb08",
@@ -100,20 +108,20 @@ self.__precacheManifest = [
     "url": "/img/apple-icon-180x180.png"
   },
   {
-    "revision": "36d4b87514744f6fc41e996963b465cd",
-    "url": "/img/android-icon-96x96.png"
-  },
-  {
     "revision": "00d81d5aeba479288f1ca750a769ab6c",
     "url": "/img/casi.00d81d5a.png"
   },
   {
-    "revision": "36d4b87514744f6fc41e996963b465cd",
-    "url": "/img/favicon-96x96.png"
+    "revision": "73ba007214e8924b997ce933c7f8f98b",
+    "url": "/img/apple-icon-76x76.png"
   },
   {
-    "revision": "f9d7a778824165b7d4d1c74386285906",
-    "url": "/img/android-icon-72x72.png"
+    "revision": "272425dc956ec4befa460c6dbf8c165a",
+    "url": "/img/android-icon-48x48.png"
+  },
+  {
+    "revision": "473a2747bb20ece3a014122b6f0eacc6",
+    "url": "/img/apple-icon-114x114.png"
   },
   {
     "revision": "54ecf212ce46474f6553bafa02256fb3",
@@ -128,10 +136,6 @@ self.__precacheManifest = [
     "url": "/img/apple-icon-144x144.png"
   },
   {
-    "revision": "fc2955fa81ee41c1a02dd17add77240b",
-    "url": "/img/android-icon-36x36.png"
-  },
-  {
     "revision": "78728730c6f498b12bbaeaf8dc0c3cfd",
     "url": "/img/android-icon-192x192.png"
   },
@@ -140,23 +144,27 @@ self.__precacheManifest = [
     "url": "/img/android-icon-144x144.png"
   },
   {
-    "revision": "39138eb71bec36414a2a",
-    "url": "/css/chunk-vendors.8dd93730.css"
+    "revision": "3183b3e7ed84ae8c08d4",
+    "url": "/css/chunk-vendors.b3d7ff6a.css"
+  },
+  {
+    "revision": "ed200db68e5327e09202",
+    "url": "/css/chunk-9acbd058.edf952fb.css"
   },
   {
     "revision": "a48632deb1f77d3f6d9a",
     "url": "/css/chunk-7c3900fd.3bed44c1.css"
   },
   {
-    "revision": "e7d62d3267ef8c62dd42",
-    "url": "/css/chunk-59b24cff.3bbca633.css"
+    "revision": "c2835fc693502b023963",
+    "url": "/css/chunk-40d08593.d15fed7f.css"
   },
   {
     "revision": "d491218fbe6ebf6f98c9",
     "url": "/css/chunk-1a4d6c1c.d4e60883.css"
   },
   {
-    "revision": "94448cfa72594d756f53",
+    "revision": "5cae1215b884e9d29210",
     "url": "/css/app.69811be8.css"
   }
 ];
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8af7eda1f88e2c6d8bf8902c2e01dcc0.js"
+  "/precache-manifest.82944080d3ed904573a7ae5d5dc2eb88.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "xi-sacsis-website"});
