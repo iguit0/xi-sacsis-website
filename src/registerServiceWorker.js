8b1c7da31762aaadfbae36cb6f6513b38c9ece69
Igor Alves <igorsk89@gmail.com>
2019-05-27 11:06:10 -0300

    service worker

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -9,10 +9,6 @@ if (process.env.NODE_ENV === 'production') {
         'App is being served from cache by a service worker.\n' +
         'For more details, visit https://goo.gl/AFskqB'
       )
-    },
-    updated() {
-      setTimeout(function () { window.location.reload() }, 1000);
-      console.log('New content is available; please refresh.')
     }
   })
 }
